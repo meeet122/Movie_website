04421fca8a9ff93e19cbbdc4fad986fca4a5ac93
Meet Patel <meeet122@gmail.com>
2017-07-16 10:22:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import fresh_tomatoes
 import webbrowser
-import media
+
 
 Tubelight = media.Movie("Tubelight",
                         "A war time drama film",
                         "https://en.wikipedia.org/wiki/File:Tubelight_Poster.jpg",
                         "https://www.youtube.com/watch?v=PGQRNKHJwH4")
-
-#Tubelight.show_trailer()
-#Tubelight.show_poster()
 movies = [Tubelight]
 fresh_tomatoes.open_movies_page(movies)
 
